7e154a20a7706e0ec4050213ecaa12b51d28539d
hyper <hyperskill@hyperskill.org>
2024-02-24 16:12:06 +0600

    Stage 2/6

--- Quiz.java
+++ Quiz.java
@@ -1,24 +1,40 @@
 package engine;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Quiz {
+    private int id;
     private String title;
     private String text;
     private List<String> options;
-    public final static Quiz quizQuestion = new Quiz("The Java Logo",
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private int answer;
+    public final static Quiz quizQuestion = new Quiz(100, "The Java Logo",
             "What is depicted on the Java logo?",
             Arrays.asList("Robot","Tea leaf","Cup of coffee","Bug"));
 
     public Quiz() {
     }
 
-    public Quiz(String title, String text, List<String> options) {
+    public Quiz(int id, String title, String text, List<String> options) {
+        this.id = id;
         this.title = title;
         this.text = text;
         this.options = options;
+    }
+
+    public Quiz(int id, String title, String text, List<String> options, int answer) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.options = options;
+        this.answer = answer;
     }
 
     public String getTitle() {
@@ -43,5 +59,22 @@ public class Quiz {
 
     public void setOptions(List<String> options) {
         this.options = options;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
     }
 }
--- QuizController.java
+++ QuizController.java
@@ -1,12 +1,18 @@
 package engine;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @RestController
 public class QuizController {
+    int counter = 1;
+    List<Quiz> quizzes = new ArrayList<>();
     @GetMapping("/api/quiz")
     public Quiz getQuiz() {
         return Quiz.quizQuestion;
@@ -19,5 +25,42 @@ public class QuizController {
         } else {
             return QuizAnswer.wrongAnswer;
         }
+    }
+
+    @PostMapping("/api/quizzes")
+    public Quiz createAndReturnUserQuiz(@RequestBody Quiz quiz) {
+        quiz.setId(counter);
+        counter++;
+        quizzes.add(quiz);
+        return quiz;
+    }
+
+    @GetMapping("api/quizzes/{id}")
+    public ResponseEntity<?> getQuizById(@PathVariable int id) {
+        for (Quiz quiz : quizzes) {
+            if (quiz.getId() == id) {
+                return ResponseEntity.ok(quiz);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("api/quizzes")
+    public ResponseEntity<?> getAllQuizzes() {
+        return ResponseEntity.ok(quizzes);
+    }
+
+    @PostMapping("api/quizzes/{id}/solve")
+    public ResponseEntity<?> returnAnswer(@PathVariable int id, @RequestParam int answer) {
+        for (Quiz quiz : quizzes) {
+            if (quiz.getId() == id) {
+                if (quiz.getAnswer() == answer) {
+                    return ResponseEntity.ok(QuizAnswer.rightAnswer);
+                } else {
+                    return ResponseEntity.ok(QuizAnswer.wrongAnswer);
+                }
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 }
